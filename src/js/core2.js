bb30baac9a1d1adf776b5687afba9254a2cac568
Attila Tóth <attila@symbolic-trading.com>
2018-12-03 13:04:48 +0100

    new colours

--- src/js/core2.js
+++ src/js/core2.js
@@ -1,3 +1,4 @@
+/* THIS FILE IS ONLY HERE FOR THE DEMO!! NO time for making it look nicer!!! */
 const appFactory = require('./app-factory')
 
 // HTML elements
@@ -8,7 +9,7 @@ const yieldedInfoDiv = document.getElementById('yielded-messages')
 const yieldTrigger = document.getElementById('yield-button')
 const yieldInfo = document.getElementById('yield-info')
 
-const graph = require('../graphs/graph1')
+const graph = require('../graphs/graph2')
 
 appFactory({
   cyContainer,
--- src/misc/style-factory.js
+++ src/misc/style-factory.js
@@ -1,48 +1,67 @@
-module.exports = [
-  {
-    selector: 'node',
-    style: {
-      'label': 'data(id)',
-      'background-color': 'gray',
-    }
-  },
-  {
-    selector: 'node.cluster-unsigned',
-    style: {
-      'background-color': '#ff1d00',
-      'label': 'data(id)'
-    }
-  },
-  {
-    selector: 'edge',
-    style: {
-      'label': 'data(id)'
-    }
-  },
-  {
-    selector: 'edge.final',
-    style: {
-      'label': 'data(id)',
-      'line-color': '#2ead00',
-    }
-  },
-  {
-    selector: 'edge.red-edge',
-    style: {
-      'label': 'data(weight)',
-      'line-color': '#ff1d00',
-    }
-  }
-].concat([
+const randomColor = require('./random-color')
+module.exports = nodeNumber => {
+  const defClusterColors = [
     '#001c49',
     '#f4df42',
     '#00576d',
     '#98f442',
-    '#f4ad49'
-  ].map((x, i) => ({
-    selector: `node.cluster-${ i }`,
-    style: {
-      'background-color': x,
+    '#f4ad49',
+  ]
+  const colors = [
+    {
+      selector: 'node',
+      style: {
+        'label': 'data(id)',
+        'background-color': 'gray',
+      }
+    },
+    {
+      selector: 'node.cluster-unsigned',
+      style: {
+        'background-color': '#ff1d00',
+        'label': 'data(id)'
+      }
+    },
+    {
+      selector: 'edge',
+      style: {
+        'label': 'data(id)'
+      }
+    },
+    {
+      selector: 'edge.final',
+      style: {
+        'label': 'data(id)',
+        'line-color': '#2ead00',
+      }
+    },
+    {
+      selector: 'edge.red-edge',
+      style: {
+        'label': 'data(weight)',
+        'line-color': '#ff1d00',
+      }
     }
-  })))
+  ].concat(defClusterColors.map((x, i) => ({
+      selector: `node.cluster-${ i }`,
+      style: {
+        'background-color': x,
+      }
+    })))
+
+  const diff = nodeNumber - defClusterColors.length
+  if (diff > 0) {
+    for (let i = 0; i < diff; i++) {
+      colors.push({
+        selector: `node.cluster-${ defClusterColors.length + i }`,
+        style: {
+          'background-color': randomColor(),
+        }
+      })
+    }
+  }
+
+  console.info('colors: ', colors)
+  return colors
+}
 
